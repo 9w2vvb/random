11c9d0ec88992afcb9f7d2887a6f5545e7975264
9w2vvb <9w2vvb@gmail.com>
2017-03-19 16:35:41 +0800

    add working script

--- 2017/exchange/get-mailbox-size-and-quota-working.ps1
+++ 2017/exchange/get-mailbox-size-and-quota-working.ps1
@@ -1,11 +1,12 @@
 # working!
-
-$users = Import-Csv .\user.csv
+#
+$users = Import-Csv user.csv
 @(foreach ($user in $users) {
-    Get-Mailbox $user.SamAccountName | Select-Object DisplayName, @{name="TotalItemSize(MB)";expression={(get-mailboxstatistics $_).TotalItemSize.Value.ToMB()}}, *quota*
+    Get-Mailbox $user.SamAccountName | Select-Object DisplayName, @{name="TotalItemSize(MB)";expression={(Get-MailboxStatistics $_).TotalItemSize.Value.ToMB()}}, *quota*
 }) | Export-Csv UserMailboxSizes.csv -NoTypeInformation
 
-# format of csv file:
+# format of user.csv file:
+#
 # SamAccountName
 # user1
 # user2
